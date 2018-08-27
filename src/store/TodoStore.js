7f9c07dc22229c3579232f3ada5bee29d273de65
Apolo <apolo@192.168.0.100>
2018-08-28 01:16:58 +0800

    compile ES7 class property with babel plugin transform-class-properties

--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -2,22 +2,16 @@ import {  decorate, observable, autorun} from 'mobx'
 import React from 'react'
 
 class TodoStore{
-  constructor(){
-    this.todos = ["buy milk", "buy food"]
-    this.filter = ''
-  }
+  todos = ["buy milk", "buy food"]
+  filter = ''
 }
 
 // 实现 console 访问 store 对象
 let store = window.store = new TodoStore()
 
-// 使用 decorate() 包装store
-
 // 使用示例:
 // decorate(stateObj, {
 //  key1: observable,
-//  key2: computed,
-//  key3: action
 // })
 
 decorate(store, {
@@ -25,10 +19,9 @@ decorate(store, {
   filter: observable,
 })
 
-export default store
-
 autorun(()=>{
   console.log("autorun is triggered")
   console.log(store.todos[0])
 })
 
+export default store
